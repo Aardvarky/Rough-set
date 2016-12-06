996fb00169529e62874e3bed55e8c759815f1857
Aardvarky <kopko.mateusz@gmail.com>
2016-12-06 21:52:05 +0100

    creating removeRules method and test them

--- RoughSet/CRuleSet.py
+++ RoughSet/CRuleSet.py
@@ -1,36 +1,89 @@
-class CRuleSet:
+from CRule import CRule
+
+
+class CRuleSet(list):
     def __init__(self):
-        pass
+        self.__ruleArray = []
 
     def copyInstance(self, RuleSet):
-        pass
+        if isinstance(RuleSet, self.__class__):
+            for i in self.__ruleArray:
+                del i
+
+            for i in RuleSet.getRuleArray():
+                self.__ruleArray.append(i)
+        else:
+            raise TypeError(RuleSet)
+
+    def append(self, rule):
+        if isinstance(rule, CRule):
+            self.__ruleArray.append(rule)
+        else:
+            raise TypeError(rule)
+
+    def getRuleArray(self):
+        return self.__ruleArray
+
+    def setRuleArray(self, RuleArray):
+        self.__ruleArray = RuleArray
 
     def assignOperator(self, RuleSet):
-        pass
+        self.copyInstance(RuleSet)
+        return self
 
     def setParameterForRules(self, name, value):
-        pass
+        for i in self.__ruleArray:
+            i.setParameter(name, value)
 
     def removeRules(self, mode):
-        pass
+        i = 0
+        j = 0
+
+        while i < len(self.__ruleArray):
+            j = i + 1
+            while j < len(self.__ruleArray):
+                if self.__ruleArray[i].__eq__(self.__ruleArray[j]):
+                    if mode == 0x01:
+                        self.__ruleArray[i].increaseSupport(self.__ruleArray[j].getSupport())
+                    self.__ruleArray.remove(self.__ruleArray[i])
+                    j -= 1
+                j += 1
+            i += 1
 
     def removeWeakRules(self, minStrength):
-        pass
+        if type(minStrength) == float:
+            newListOfRules = []
+
+            f = lambda x : x.getStrength() < minStrength
+            booleanCheckList = list(map(f, self.__ruleArray))
+
+            for i, (x, y) in enumerate(zip(self.__ruleArray, booleanCheckList)):
+                if not y:
+                    newListOfRules.append(x)
+
+            self.setRuleArray(newListOfRules)
+        else:
+            raise TypeError(minStrength)
 
     def computeCosts(self, costArray):
-        pass
+        for i in self.__ruleArray:
+            i.computeCost(costArray)
 
     def computeStrength(self, objectNumber):
-        pass
+        for i in self.__ruleArray:
+            i.computeStrength(objectNumber)
 
     def computeCoverage(self, valuesOfAttr):
-        pass
+        for i in self.__ruleArray:
+            i.computeCoverage(valuesOfAttr)
 
     def computeCertainty(self):
-        pass
+        for i in self.__ruleArray:
+            i.computeCertainty()
 
     def computeSignatures(self):
-        pass
+        for i in self.__ruleArray:
+            i.computeSignature()
 
     def compInhibRules(self, attrValues, inhibRules):
         pass
--- RoughSet/test.py
+++ RoughSet/test.py
@@ -1,40 +1,95 @@
-from CInformTable import CInformTable
-from CGlobalState import CGlobalState
-from BasicInformSystem import BasicInformSystem
-from CImplicant import CImplicant
-from CImplArray import CImplArray
 from CRule import CRule
 from HashableDict import HashableDict
-
+from random import randint
+from CRuleSet import CRuleSet
 
 consequent = HashableDict()
-consequent.add(1, 1.0)
-consequent.add(2, 2.0)
-consequent.add(3, 3.0)
+consequent.add(randint(0, 100), 1.0)
+consequent.add(randint(0, 100), 2.0)
+consequent.add(randint(0, 100), 3.0)
+consequent.add(randint(0, 100), 4.0)
 
 consequent1 = HashableDict()
-consequent1.add(1, 1.0)
-consequent1.add(2, 2.0)
-consequent1.add(3, 3.0)
+consequent1.add(100, 1.0)
+consequent1.add(200, 2.0)
+consequent1.add(300, 3.0)
+consequent1.add(400, 4.0)
+
+consequent2 = HashableDict()
+consequent2.add(100, 1.0)
+consequent2.add(200, 2.0)
+consequent2.add(300, 3.0)
+consequent2.add(400, 4.0)
+
+consequent3 = HashableDict()
+consequent3.add(400, 1.0)
+consequent3.add(500, 2.0)
+consequent3.add(600, 3.0)
+consequent3.add(700, 4.0)
+
+consequent4 = HashableDict()
+consequent4.add(400, 1.0)
+consequent4.add(500, 2.0)
+consequent4.add(600, 3.0)
+consequent4.add(700, 4.0)
 
 antecedent = HashableDict()
-antecedent.add(1, 1.0)
-antecedent.add(2, 2.0)
-antecedent.add(3, 3.0)
+antecedent.add(randint(0, 100), 1.0)
+antecedent.add(randint(0, 100), 2.0)
+antecedent.add(randint(0, 100), 3.0)
+antecedent.add(randint(0, 100), 4.0)
 
 antecedent1 = HashableDict()
-antecedent1.add(1, 1.0)
-antecedent1.add(2, 2.0)
-antecedent1.add(3, 3.0)
+antecedent1.add(10, 1.0)
+antecedent1.add(20, 2.0)
+antecedent1.add(30, 3.0)
+antecedent1.add(40, 4.0)
+
+antecedent2 = HashableDict()
+antecedent2.add(10, 1.0)
+antecedent2.add(20, 2.0)
+antecedent2.add(30, 3.0)
+antecedent2.add(40, 4.0)
+
+antecedent3 = HashableDict()
+antecedent3.add(50, 1.0)
+antecedent3.add(60, 2.0)
+antecedent3.add(70, 3.0)
+antecedent3.add(80, 4.0)
+
+antecedent4 = HashableDict()
+antecedent4.add(50, 1.0)
+antecedent4.add(60, 2.0)
+antecedent4.add(70, 3.0)
+antecedent4.add(80, 4.0)
 
 cRule = CRule()
 cRule.setConsequent(consequent)
 cRule.setAntecedent(antecedent)
 
-
 cRule1 = CRule()
 cRule1.setConsequent(consequent1)
 cRule1.setAntecedent(antecedent1)
 
+cRule2 = CRule()
+cRule2.setConsequent(consequent2)
+cRule2.setAntecedent(antecedent2)
 
-print(cRule.equalOperator(cRule1))
+cRule3 = CRule()
+cRule3.setConsequent(consequent3)
+cRule3.setAntecedent(antecedent3)
+
+cRule4 = CRule()
+cRule4.setConsequent(consequent4)
+cRule4.setAntecedent(antecedent4)
+
+cRuleSet = CRuleSet()
+cRuleSet.append(cRule)
+cRuleSet.append(cRule1)
+cRuleSet.append(cRule2)
+cRuleSet.append(cRule3)
+cRuleSet.append(cRule4)
+
+print(cRuleSet.getRuleArray())
+cRuleSet.removeRules(1)
+print(cRuleSet.getRuleArray())
