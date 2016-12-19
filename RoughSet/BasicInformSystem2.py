8b982c883c0bf3a060759b491cd8bbdc436bea18
Aardvarky <kopko.mateusz@gmail.com>
2016-12-19 22:59:42 +0100

    BasicInformSystem2

---
+++ RoughSet/BasicInformSystem2.py
@@ -0,0 +1,73 @@
+from BasicInformSystem import BasicInformSystem
+
+class BasicInformSystem2(BasicInformSystem):
+    """ Basic Information System 2"""
+
+    def __init__(self):
+        super().__init__()
+
+    def setObjectNumber(self, objectNumber):
+        super().setObjectNumber(objectNumber)
+
+    def setAttributeNumber(self, attributeNumber):
+        super().setAttributeNumber(attributeNumber)
+
+    def setAttributeIndexes(self, attributeIndexes):
+        super().setAttributeIndexes(attributeIndexes)
+
+    def compDependDegreeForAttribute(self, conAttr, decAttr):
+        return super().compDependDegreeForAttribute(conAttr, decAttr)
+
+    def compNondetRules(self, conAttr, decAttr):
+        return super().compNondetRules(conAttr, decAttr)
+
+    def getInformTable(self):
+        return super().getInformTable()
+
+    def getAttributeName(self, index):
+        return super().getAttributeName(index)
+
+    def compDiscernMatrix(self):
+        super().compDiscernMatrix()
+
+    def getAttributeNames(self):
+        return super().getAttributeNames()
+
+    def setName(self, name):
+        super().setName(name)
+
+    def getName(self):
+        return super().getName()
+
+    def getAttributeTypes(self):
+        return super().getAttributeTypes()
+
+    def getObjectName(self, index):
+        return super().getObjectName(index)
+
+    def getObject(self, index):
+        return super().getObject(index)
+
+    def getSystemType(self):
+        return super().getSystemType()
+
+    def getAttributeType(self, index):
+        return super().getAttributeType(index)
+
+    def compDependDegreeForAttributes(self, conAttr, decAttributes):
+        return super().compDependDegreeForAttributes(conAttr, decAttributes)
+
+    def setInformTable(self, informTable):
+        super().setInformTable(informTable)
+
+    def setAttributeNames(self, attributeNames):
+        super().setAttributeNames(attributeNames)
+
+    def getObjectNumber(self):
+        return super().getObjectNumber()
+
+    def getAttributeNumber(self):
+        return super().getAttributeNumber()
+
+    def setSystemType(self, systemType):
+        super().setSystemType(systemType)
--- RoughSet/CBasicInformSystem2.py
+++
@@ -1,8 +0,0 @@
-class CBasicInformSystem2:
-    """ Basic Information System 2"""
-
-    def __init__(self):
-        pass
-
-    def __del__(self):
-        pass
